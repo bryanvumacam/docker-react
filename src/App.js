8cb720256dd859b6ab42996d9e74e96c89ecc7ad
Bryan Vergara Umacam <bryanvumacam@Bryans-MacBook-Pro.local>
2022-04-20 12:07:57 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Chyn Chyn Pasaway na Bata
         </p>
         <a
           className="App-link"
